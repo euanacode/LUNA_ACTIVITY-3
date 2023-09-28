28a5cbd2fbf515c2ae7c83957eb24337dd0d5122
euanacode <144924511+euanacode@users.noreply.github.com>
2023-09-28 19:13:28 +0800

    Add files via upload
    I apologize for the late pass my laptop is laggy when it comes to big programs like this.

---
+++ LUNA_SalesTaxActivity.py
@@ -0,0 +1,8 @@
+# for purchasing purposes
+PurchaseAmount = eval(input("Enter Purchase Amount:"))
+SalesTax = float(0.06)
+Amount = (PurchaseAmount * SalesTax)
+
+print()
+print("Sales Tax:" + str(Amount))
+
